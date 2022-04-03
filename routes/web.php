5b2aae07c256594c8b6d5ed7266287589dce2975
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-04-04 00:22:09 +0300

    f

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
+// Route::get('/home', function () {
+//     return view('home');
+// });
 Route::get('/home',[homeController::class,'homef']);
